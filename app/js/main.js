6bc4fb17c75cb9631a43cfe2895fe9f37fd80e56
TanyaSummer <89299874440@mail.ru>
2021-06-16 22:55:16 +0300

    add shop_page

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,33 @@
 $(function(){
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data){
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+  },
+
+  });
+
+
   $('.top-slider__inner').slick({
     dots:true,
     arrows:false
   });
  
-  // $('[data-fancybox]').fancybox({
-  //   youtube: {
-  //     controls: 1
-  //   }
-  // });
+  $(".star").rateYo({
+    
+    starWidth: "10px",
+    normallFill: "#d6d6d6",
+    ratedFill: '#ffc35b',
+    // readOnly: true
+  });
 
   var mixer = mixitup('.products__items');
   var mixer = mixitup('.new-disign__items');  
